f469d28cbf3ce594e01514bae1c4c7fe27a29222
Jason Reid <jason.reid@r3labs.io>
2017-03-02 13:07:29 +0000

    rebuilding site Thu  2 Mar 2017 13:07:29 GMT

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-cd public && git commit -am "Automated deploy" && git push origin gh-pages
-# git subtree push --prefix=public git@github.com:ernestio/ernest-site.git gh-pages
+# cd public && git commit -am "Automated deploy" && git push origin gh-pages
+git subtree push --prefix=public git@github.com:ernestio/ernest-site.git gh-pages
